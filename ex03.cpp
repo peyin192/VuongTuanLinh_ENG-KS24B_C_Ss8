ad4e15f5cd0ea001d5c24eae263fae7b143ebcd0
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-28 22:48:11 +0700

    upload ss08-3

---
+++ ex03.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>;
+int main(){
+	int n;
+	
+	printf("Nhap vao 1 so nguyen: ");
+	scanf("%d",&n);
+	
+		int arr[3][3];
+		
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			printf(" %d ",n);
+		}
+		printf("\n");
+	}
+	return 0;
+
+	
+	
+	
+	
+}
