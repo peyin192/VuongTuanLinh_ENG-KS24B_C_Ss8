4e68321edb623192469392255cff7761627a2532
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-29 13:59:42 +0700

    upload ss08-10

---
+++ ex9.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int arr[]={1,2,1,5,7,2,4};
+	int maxCount;
+	for(int i=0;i<7;i++){
+		for(int j=0;j<7;j++){
+			if(arr[i]==arr[]){
+				count++;
+			}
+		}
+	}
+	if(count>maxCount){
+		count = maxCount;
+	}
+}
